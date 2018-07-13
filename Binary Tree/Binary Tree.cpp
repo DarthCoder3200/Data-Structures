94f57825c811fb86d5fc6800305bb7fc63d75e53
Harjot Singh <harjot3200@gmail.com>
2018-07-14 00:43:34 +0530

    Binary Tree added

---
+++ Binary Tree/Binary Tree.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node{
+public:
+
+int data;
+node*right;
+node*left;
+node(int d){
+data = d;
+right = NULL;
+left = NULL;
+}
+};
+int max(int a, int b){
+if(a>=b){
+    return a;
+}
+else{
+    return b;
+}
+}
+
+node*buildTree(){
+int d;
+cin>>d;
+if(d==-1){
+    return NULL;
+}
+node*n = new node(d);
+n->left  = buildTree();
+n->right = buildTree();
+return n;
+}
+void printPreOrder(node*root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+}
+void printLevel(node*root, int k){
+    if(root == NULL || k<0){
+        return;
+    }
+    if(k == 0){
+        cout<<root->data<<" ";
+        return;
+    }
+    printLevel(root->left,k-1);
+    printLevel(root->right,k-1);
+}
+int countnodes(node*root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1 + countnodes(root->left) + countnodes(root->right);
+}
+int height(node*root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1 + max(height(root->left),height(root->right));
+}
+void levelOrderPrint(node*root){
+
+    for(int i = 0 ;i<height(root);i++){
+        printLevel(root,i);
+        cout<<endl;
+    }
+    return;
+}
+void inOrderPrint(node*root){
+    if(root == NULL){
+        return;
+    }
+    inOrderPrint(root->left);
+    cout<<root->data<<" ";
+    inOrderPrint(root->right);
+    return;
+}
+
+void postOrderPrint(node*root){
+    if(root == NULL){
+        return;
+    }
+    postOrderPrint(root->left);
+    postOrderPrint(root->right);
+    cout<<root->data<<" ";
+    return;
+}
+
+int diameter (node*root){
+    if(root == NULL){
+        return 0;
+    }
+    int op1 = height(root->left) + height(root->right);
+    int op2 = diameter(root->left);
+    int op3 = diameter(root->right);
+
+    return max(op1,max(op2,op3));
+}
+
+void levelOrderInput(node*&root){
+    cout<<"Enter the root node";
+    int d;
+    cin>>d;
+    root = new node(d);
+
+    queue<node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        node*parent = q.front();
+        q.pop();
+        cout<<"Enter the children of "<<parent->data;
+        int c1;
+        int c2;
+        cin>>c1;
+        cin>>c2;
+        if(c1 != -1){
+            parent->left = new node(c1);
+            q.push(parent->left);
+        }
+        if(c2 != -1){
+            parent->right = new node(c2);
+            q.push(parent->right);
+        }
+    }
+}
+
+int main(){
+    node*root = NULL;
+    /*printPreOrder(root);
+    cout<<endl;
+    printLevel(root,2);
+    cout<<endl;
+    cout<<countnodes(root);
+    cout<<endl;
+    cout<<height(root);
+    cout<<endl;*/
+    levelOrderInput(root);
+    levelOrderPrint(root);
+    return 0;
+}
